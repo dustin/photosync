018748ca1e9834970a9ce95765e59902c5a5c411
Dustin Sallings <dustin@spy.net>
2005-02-04 05:22:43 +0000

    the message definitions

---
+++ PhotoSync.h
@@ -0,0 +1,11 @@
+/*
+ *  PhotoSync.h
+ *  PhotoSync
+ *
+ *  Created by Dustin Sallings on 2005/2/3.
+ *  Copyright 2005 Dustin Sallings. All rights reserved.
+ *
+ */
+
+#define PS_DID_UPDATE @"PS_DidUpdate"
+#define PS_TASK_COMPLETE @"PS_TaskComplete"
